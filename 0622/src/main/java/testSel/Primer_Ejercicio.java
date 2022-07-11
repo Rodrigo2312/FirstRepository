ed05752c5ac9b6d825a6f78560690d67f7aa9b8b
Rodrigo <Rodrigo@DESKTOP-3VDS6S3>
2022-07-11 14:52:35 -0300

    Primer Ejercicio

---
+++ 0622/src/main/java/testSel/Primer_Ejercicio.java
@@ -0,0 +1,83 @@
+package testSel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Primer_Ejercicio {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		String exePath = "./driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com");
+		driver.manage().window().maximize();
+		
+		WebElement userName = driver.findElement(By.id ("txtUsername"));
+		WebElement password = driver.findElement(By.id("txtPassword"));
+		WebElement btnLogin = driver.findElement(By.id("btnLogin"));
+		
+		//Login
+		userName.sendKeys("Admin");
+		Thread.sleep(2000);
+		password.sendKeys("admin123");
+		Thread.sleep(2000);
+		btnLogin.click();
+		
+		//Click en Directory
+		WebElement btnDirectory = driver.findElement(By.id("menu_directory_viewDirectory"));
+		btnDirectory.click();
+		
+		//Ingresar nombre Nathan
+		WebElement directoryName = driver.findElement(By.id("searchDirectory_emp_name_empName"));
+		directoryName.sendKeys("Nathan");
+		Thread.sleep(2000);
+		
+		//Click en Search
+		WebElement btnSearch = driver.findElement(By.id("searchBtn"));
+		btnSearch.click();
+		
+		//Seleccionar Job Title
+		Select drpJobTitle = new Select(driver.findElement(By.id("searchDirectory_job_title")));
+		drpJobTitle.selectByVisibleText("Sales Representative");
+		
+		//Seleccionar Location
+		Select drpLocation = new Select(driver.findElement(By.id("searchDirectory_location")));
+		drpLocation.selectByVisibleText("United States");
+		
+		//Click en Search
+		WebElement btnSearch1 = driver.findElement(By.id("searchBtn"));
+		btnSearch1.click();
+		
+		//Verificacion de Name completo
+		String nameVerification = driver.findElement(By.xpath("//*[@id=\"resultTable\"]/tbody/tr[2]/td[2]/ul/li[1]/b")).getText();
+		boolean name = nameVerification.contains("Nathan Elliot");
+		
+		if(name) {
+			System.out.println("Se encontro a Nathan correctamente");
+		}else {
+			System.out.println("ERROR: Nathan no fue encontrado");
+		}
+		
+		/*
+		
+		
+		//Obtener el texto
+		
+		String welcomeName = driver.findElement(By.id("welcome")).getText();
+		boolean name = welcomeName.contains("kota");
+		
+		if(name) {
+			System.out.println("El mensaje contiene el nombre de kathija");
+		}else {
+			System.out.println("ERROR: el Mensaje no tiene el nombre kathija");
+		}
+		
+		*/
+	}
+
+}
+
